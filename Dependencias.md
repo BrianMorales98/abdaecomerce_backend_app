88262b5664470d0baebfb4f70e8a5b7d544080ff
Cruzade-ab <abimael.santa1@gmail.com>
2024-02-27 16:20:47 -0400

    progreso jwt

--- Dependencias.md
+++ Dependencias.md
@@ -2,4 +2,6 @@
 
 1- Bcrypt
 2- Types/bcrypt
-
+3- @nestjs/jwt 
+4- passport-jwt
+5- @types/passwort-jwt
--- src/user/user.Controller.ts
+++ src/user/user.Controller.ts
@@ -1,7 +1,8 @@
 // Importaciones de módulos y servicios necesarios
-import { Body, Controller, Post } from "@nestjs/common";
 import { UserService } from "./user.service";
 import { User } from "@prisma/client";
+import { Controller, Post, Body, Res, HttpStatus } from '@nestjs/common';
+import { Response } from 'express';
 
 // Decorador @Controller define la ruta base para las rutas manejadas por este controlador
 @Controller('api') 
@@ -21,8 +22,17 @@ export class UserController {
 
     @Post('login')
     // Metod para manejar la solicitud de login 
-    async loginUser(@Body() data: User){
-        console.log(data)
-        return this.UserService.loginUser(data)
+    async loginUser(@Body() data: User, @Res({passthrough: true}) res: Response){
+        try {
+            const token = await this.UserService.loginUser(data);
+            
+            res.cookie('token', token, {httpOnly: true});
+
+            return{
+                message: "succes"
+            }
+        } catch (error) {
+            message: "Invalid Credentials"
+        }
     }
 }
