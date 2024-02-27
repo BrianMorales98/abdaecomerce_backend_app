9def86d8c8700f212d2e69491ad6033f1ca4b1fe
Abima <abimael.santa1@gmail.com>
2024-02-27 18:37:11 -0400

    Codigo Formateado y refactorizado para seguir las buenas praxtixas- Funcionalidad JWT:Cookies Register/Login

--- src/user/user.Controller.ts
+++ src/user/user.Controller.ts
@@ -1,85 +1,37 @@
 // Importaciones de módulos y servicios necesarios
 import { UserService } from "./user.service";
 import { User } from "@prisma/client";
-import { Controller, Post, Body, Res, HttpStatus, Get, Req, UnauthorizedException } from '@nestjs/common';
-import { Response, Request, CookieOptions, response } from 'express';
-import { JwtService } from "@nestjs/jwt";
-import { PrismaService } from "src/prisma/prisma.service";
+import { Controller, Post, Body, Res, Get, Req } from '@nestjs/common';
+import { Response, Request } from 'express';
 
-// Decorador @Controller define la ruta base para las rutas manejadas por este controlador
 @Controller('api') 
 export class UserController {
 
-    // Constructor del controlador que inyecta el servicio necesario para manejar el registro de usuarios
     constructor(
-        private readonly UserService: UserService,
-        private jwtService: JwtService,
-        private prisma: PrismaService,
-        ) {}
+        private readonly userService: UserService,
+    ) {}
 
-    // Decorador @Post define una ruta POST para manejar el registro de usuarios
-    @Post('register')
     // Método para manejar la solicitud de registro de usuarios
+    @Post('register')
     async registerUser(@Body() data: User) {
-        console.log(data)
-
-        const user = await this.UserService.createUser(data);
-
-        const { password, ...userData} = user
-
-        return userData
+        return this.userService.registerUser(data);
     }
 
+    // Método para manejar la solicitud de inicio de sesión de usuarios
     @Post('login')
-    // Metod para manejar la solicitud de login 
-    async loginUser(@Body() data: User, @Res({passthrough: true}) res: Response){
-        try {
-            const token = await this.UserService.loginUser(data);
-            
-            res.cookie('token', token, {httpOnly: true});
-
-            return{
-                message: "Succes"
-            }
-        } catch (error) {
-            message: "Invalid Credentials"
-        }
+    async loginUser(@Body() data: User, @Res({passthrough: true}) res: Response) {
+        return this.userService.loginUser(data, res);
     }
 
-     @Get('user')
-    async user(@Req() request: Request) {
-        let token: any;
-        try {
-
-            const cookie = request.cookies['token'];
-            if (cookie) {
-                token = cookie; // Assuming the entire cookie is the token
-            } else {
-                throw new Error("JWT token not found in the cookie");
-            }
-
-            const data = await this.jwtService.verifyAsync(token)
-
-            if(!data) {
-                throw new UnauthorizedException()
-            }
-
-            const user = await this.prisma.user.findFirst({where: {email: data.email}})
-
-            const {password, ...userData} = user
-
-            return userData;
-
-        } catch (error) {
-            throw new UnauthorizedException()
-        }        
+    // Método para manejar la solicitud de obtención de información de usuario
+    @Get('user')
+    async getUser(@Req() request: Request) {
+        return this.userService.getUserFromToken(request);
     }
 
+    // Método para manejar la solicitud de cierre de sesión de usuarios
     @Post('logout')
-    async userLogout(@Res({passthrough: true}) response: Response) {
-        response.clearCookie('token')
-        return {
-            message: "Succes"
-        }
+    async logoutUser(@Res({passthrough: true}) response: Response) {
+        return this.userService.logoutUser(response);
     }
 }
--- src/user/user.Service.ts
+++ src/user/user.Service.ts
@@ -1,50 +1,115 @@
-import { BadRequestException, Injectable } from "@nestjs/common";
+// Importaciones de módulos y servicios necesarios
+import { BadRequestException, Injectable, UnauthorizedException } from "@nestjs/common";
 import { PrismaService } from "src/prisma/prisma.service";
-import { User} from  "@prisma/client"
-import * as bcrypt from "bcrypt"
+import { User } from "@prisma/client";
+import * as bcrypt from "bcrypt";
 import { JwtService } from "@nestjs/jwt";
-
+import { Response, Request } from "express";
 
 @Injectable()
-export class UserService{
+export class UserService {
 
     constructor(
-        private prisma: PrismaService,
-        private jwtService: JwtService){}
-    
+        private readonly prisma: PrismaService,
+        private readonly jwtService: JwtService
+    ) {}
 
-    async createUser(data: User): Promise<User>{
-        data['role_id'] = 1 
+    // Método para registrar un nuevo usuario
+    async registerUser(data: User): Promise<User> {
+        // Asigna un valor predeterminado para el role_id
+        data['role_id'] = 1;
 
-         //Se crea una variable que contenga el valor del password pero encryptado
-         const hashedPassword = await bcrypt.hash( data.password, 12);
- 
-         //Se asigna ese valor encriptado a la propiedad password del data object
-         data.password = hashedPassword; 
- 
-         // Llama al método createUser del servicio registerService y retorna el resultado
+        // Encripta la contraseña
+        const hashedPassword = await bcrypt.hash(data.password, 12);
+
+        // Asigna la contraseña encriptada al objeto de datos del usuario
+        data.password = hashedPassword;
+
+        // Llama al método createUser del servicio de Prisma para crear el usuario
         return this.prisma.user.create({
-            data 
-        })
+            data
+        });
     }
 
-    async loginUser(data:User): Promise<string> {
-        const user = await this.prisma.user.findFirst({where: { email: data.email}});
+    // Método para autenticar al usuario y generar un token JWT
+    async loginUser(data: User, res: Response): Promise<any> {
+        // Valida las credenciales del usuario
+        const user = await this.validateUser(data);
 
-        if(!user) {
-            throw new BadRequestException('Invalid Credentials')
+        if (!user) {
+            throw new UnauthorizedException('Credenciales inválidas');
         }
 
-        if (!await bcrypt.compare(data.password, user.password)){
-            throw new BadRequestException('Invalid Credentials')    
-        }
+        // Genera un token JWT
+        const token = await this.generateToken(user);
 
-        const jwt = await this.jwtService.signAsync({id: user.user_id, name: user.name, lastname: user.last_name, role_Id: user.role_id, email: user.email});
+        // Establece la cookie de token en la respuesta
+        res.cookie('token', token, { httpOnly: true });
 
-        return jwt; 
+        return { message: 'Éxito' };
     }
 
+    // Método para validar las credenciales del usuario
+    async validateUser(data: User): Promise<User | null> {
+        // Busca al usuario en la base de datos por su correo electrónico
+        const user = await this.prisma.user.findFirst({
+            where: { email: data.email }
+        });
 
+        // Compara las contraseñas si el usuario existe
+        if (!user || !(await bcrypt.compare(data.password, user.password))) {
+            return null;
+        }
 
+        return user;
+    }
+
+    // Método para generar un token JWT
+    async generateToken(user: User): Promise<string> {
+        // Elimina la contraseña del usuario del objeto antes de firmar el token
+        const { password, ...userWithoutPassword } = user;
+        return this.jwtService.signAsync(userWithoutPassword);
+    }
+
+    // Método para obtener información del usuario basada en el token JWT
+    async getUserFromToken(request: Request): Promise<User | undefined> {
+        // Extrae el token JWT de la solicitud
+        const token = await this.extractTokenFromRequest(request);
+
+        if (!token) {
+            throw new UnauthorizedException('Token JWT no encontrado');
+        }
+
+        // Verifica y decodifica el token JWT
+        const decoded = await this.verifyToken(token);
+
+        // Si el token no es válido o no contiene el correo electrónico, lanza una excepción
+        if (!decoded || !decoded.email) {
+            throw new UnauthorizedException('Token JWT inválido');
+        }
+
+        // Busca al usuario en la base de datos por su correo electrónico
+        return this.prisma.user.findUnique({ where: { email: decoded.email } });
+    }
+
+    // Método para extraer el token JWT de la solicitud
+    async extractTokenFromRequest(request: Request): Promise<string | null> {
+        const cookie = request.cookies['token'];
+        return cookie || null;
+    }
+
+    // Método para verificar la validez del token JWT
+    async verifyToken(token: string): Promise<any> {
+        try {
+            return await this.jwtService.verifyAsync(token);
+        } catch (error) {
+            throw new UnauthorizedException('Token JWT inválido');
+        }
+    }
+
+    // Método para cerrar la sesión del usuario y limpiar la cookie de token
+    async logoutUser(response: Response): Promise<any> {
+        response.clearCookie('token');
+        return { message: 'Éxito' };
+    }
 }
-
